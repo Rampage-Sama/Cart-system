5744511ca20fcc82ca067256706ff986259fd46b
Ofentse Rampa <ofentse.rampa7@gmail.com>
2023-05-19 20:45:36 +0200

    changes

--- cart.js
+++ cart.js
@@ -54,37 +54,38 @@ function deleteItem(itemId){
 function generateCartItems(cartItems) {
     let itemsHTML = "";
     cartItems.forEach((item) => {
-        itemsHTML += `
-            <div class="cart-item flex items-center pb-4 border-b border-gray-100">
-                <div class="cart-item-image w-40 h-24 bg-white p-4 rounded-lg">
-                    <img class="w-full h-full object-contain" src="${item.image}">
-                </div>
-                <div class="cart-item-details flex-grow">
-                    <div class="cart-item-title font-bold text-sm text-gray-600">
-                    ${item.name}
-                    </div>
-                </div>
-                <div class="cart-item-counter w-48 flex items-center">
-                    <div data-id="${item.id}" class="cart-item-decrease text-gray-400 bg-gray-100 rounded h-6 w-6 flex justify-center items-center hover:bg-gray-200 mr-2 cursor-pointer">
-                        <i class="fas fa-chevron-left fa-xs"></i>
-                    </div>
-                    <h4 class="text-gray-400">x ${item.quantity}</h4>
-                    <div data-id="${item.id}" class="cart-item-increase text-gray-400 bg-gray-100 rounded h-6 w-6 flex justify-center items-center hover:bg-gray-200 ml-2 cursor-pointer">
-                        <i class="fas fa-chevron-right fa-xs"></i>
-                    </div>
-                </div>
-                <div class="product-price w-48 font-bold text-gray-400">
-                    ${numeral(item.price * item.quantity).format('R0,0.00')}
-                </div>
-                <div data-id="${item.id}"  class="cart-item-delete w-10 font-bold text-gray-300 cursor-pointer hover:text-gray-400">
-                    <i class="fas fa-times"></i>
-                </div>
+      itemsHTML += `
+        <div class="cart-item flex items-center pb-4 border-b border-gray-100">
+          <div class="cart-item-image w-40 h-24 bg-white p-4 rounded-lg">
+            <img class="w-full h-full object-contain" src="${item.image}">
+          </div>
+          <div class="cart-item-details flex-grow">
+            <div class="cart-item-title font-bold text-sm text-gray-600">
+              ${item.name}
             </div>
-        `
-    })
+          </div>
+          <div class="cart-item-counter w-48 flex items-center">
+            <div data-id="${item.id}" class="cart-item-decrease text-gray-400 bg-gray-100 rounded h-6 w-6 flex justify-center items-center hover:bg-gray-200 mr-2 cursor-pointer">
+              <i class="fas fa-minus fa-xs"></i>
+            </div>
+            <h4 class="text-gray-400">${item.quantity}</h4>
+            <div data-id="${item.id}" class="cart-item-increase text-gray-400 bg-gray-100 rounded h-6 w-6 flex justify-center items-center hover:bg-gray-200 ml-2 cursor-pointer">
+              <i class="fas fa-plus fa-xs"></i>
+            </div>
+          </div>
+          <div class="product-price w-48 font-bold text-gray-400">
+            ${numeral(item.price * item.quantity).format('R0,0.00')}
+          </div>
+          <div data-id="${item.id}" class="cart-item-delete w-10 font-bold text-gray-300 cursor-pointer hover:text-gray-400">
+            <i class="fas fa-times"></i>
+          </div>
+        </div>
+      `;
+    });
     document.querySelector(".cart-items").innerHTML = itemsHTML;
     createEventListeners();
-}
+  }
+  
 
 function createEventListeners() {
     let decreaseButtons = document.querySelectorAll(".cart-item-decrease");
